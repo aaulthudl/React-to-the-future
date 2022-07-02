fe59e84f96df40c1d43172786a4566dda2e278b6
Arron Ault <arron.ault@hudl.com>
2022-07-02 15:14:40 +0100

    Big tings

--- src/screens/example.js
+++ src/screens/example.js
@@ -5,29 +5,7 @@ import {
   } from '@chakra-ui/react';
 import {Nav} from '../components/bottom-nav'
 export const ExampleFetch = () => {
-  const [airQualityData, setAirQualityData] = useState([]);
-  console.log(airQualityData)
 
-  useEffect(() => {
-    async function getAirQuality() {
-      const res = await fetch(
-        'https://api.ambeedata.com/latest/by-lat-lng?lat=51.530112&lng=-0.0822173',
-        {
-          method: 'GET',
-          headers: {
-            'x-api-key':
-              '98557c7e147dce2926573a404b7404eb6dccd673bc7044e4c0d13583b5bb6392',
-            'Content-type': 'application/json',
-          },
-        }
-      );
-      if (res.ok === true) {
-        const data = await res.json();
-        setAirQualityData(data.stations);
-      }
-    }
-    getAirQuality();
-  }, []);
 
   return (
     <>
@@ -35,10 +13,6 @@ export const ExampleFetch = () => {
             <Text size="large" color="black">
             Air Quality Data:
             </Text>
-            {/* Show lat and long */}
-            {airQualityData.map(data => {
-            return <Text>{data.OZONE}</Text>;
-            })}
         </Box>
         <Nav />
     </>
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,21 +1,158 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   Box,
   Text,
-  Center,
+  Input, FormControl, FormLabel, Button,
 } from '@chakra-ui/react';
-import {Nav} from '../components/bottom-nav'
-import { Logo } from '../components/logo';
+import {Nav} from '../components/bottom-nav';
+import Chart from "react-apexcharts";
 
 export const Home = () => {
+  const [resultFetched, setResultFetched] = useState(false);
+
+  const [airQuality, setAirQuality] = useState();
+  const [postcode, setPostcode] = useState();
+
+  const options = {
+    series: [75],
+    chart: {
+      height: 350,
+      type: 'radialBar',
+      toolbar: {
+        show: false
+      }
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24
+          }
+        },
+        track: {
+          background: '#fff',
+          strokeWidth: '67%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35
+          }
+        },
+
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: '#888',
+            fontSize: '17px'
+          },
+          value: {
+            formatter: function(val) {
+              return parseInt(val);
+            },
+            color: '#111',
+            fontSize: '36px',
+            show: true,
+          }
+        }
+      }
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        type: 'horizontal',
+        shadeIntensity: 0.5,
+        gradientToColors: ['#ABE5A1'],
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: 'round'
+    },
+    labels: ['Air Quality (AQI)'],
+  };
+
+
+  const getAirQuality = useCallback(async () => {
+    const res = await fetch(
+      'https://api.ambeedata.com/latest/by-lat-lng?lat=51.530112&lng=-0.0822173',
+      {
+        method: 'GET',
+        headers: {
+          'x-api-key':
+            '98557c7e147dce2926573a404b7404eb6dccd673bc7044e4c0d13583b5bb6392',
+          'Content-type': 'application/json',
+        },
+      }
+    );
+
+    if (res.ok === true) {
+      const data = await res.json();
+      setAirQuality(data.stations);
+
+      return data.stations;
+    }
+  }, []);
+
+  const handlePostcodeChange = useCallback((event) => {
+    setResultFetched(false);
+    
+    setPostcode(event.target.value);
+  }, []);
+  
+  const handlePostcodeSubmit = useCallback(async () => {
+    const data = await getAirQuality();
+
+    if (data !== undefined) {
+      setResultFetched(true);
+    }
+  }, []);
+
     return (
         <Box minH="100vh">
             <Box textAlign="center" fontSize="xl">
                 <Box spacing={8}>
-                    <Center p={20}><Logo h="40vmin" pointerEvents="none" /></Center>
-                    <Text pt={10} bg="white">
-                        HACK to the future!
-                    </Text>
+                  <FormControl>
+                    <FormLabel htmlFor='postcodeInput'>Postcode</FormLabel>
+                    <Input id="postcodeInput" value={postcode} onChange={handlePostcodeChange} placeholder='Enter a post code...'/>
+                    <Button onClick={handlePostcodeSubmit}>Let's go</Button>
+                  </FormControl>
+
+                  { resultFetched && (
+                    <>
+                      <Text pt={10} bg="white">
+                        Air Quality: {airQuality[0]["AQI"]}
+                      </Text>
+
+                      <Chart
+                        options={options}
+                        series={[airQuality[0]["AQI"]]}
+                        type="radialBar"
+                        width="500"
+                      />
+                    </>
+                  )}
                 </Box>
              </Box>
             <Nav />
