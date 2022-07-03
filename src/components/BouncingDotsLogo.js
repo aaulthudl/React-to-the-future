3a7cccc9e8b16bfdbe41789a9777c0d234ee9091
Arron Ault <arron.ault@hudl.com>
2022-07-03 11:34:50 +0100

    Bigger tings

---
+++ src/components/BouncingDotsLogo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './BouncingDotsLogo.css';
+
+const BouncingDotsLoader = () => {
+  return (
+    <>
+      <div className="bouncing-loader">
+        <div/>
+        <div/>
+        <div/>
+      </div>
+    </>
+  );
+};
+
+export default BouncingDotsLoader;
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,22 +1,28 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import {
   Box,
   Text,
   Input,
   FormControl,
   FormLabel,
-  Button,
+  Button, Img,
 } from '@chakra-ui/react';
 import Chart from 'react-apexcharts';
 import Geocode from 'react-geocode';
 import './home.css';
+import openWindowImage from '../gfx/open_window.png';
+import BouncingDotsLoader from '../components/BouncingDotsLogo'; // with import
 
 export const Home = () => {
-  // true = open, false = don't open, undefined = waiting for recommendation
-  const [shouldOpenWindow, setShouldOpenWindow] = useState(undefined);
+  // true = open, false = don't open
+  const [shouldOpenWindow, setShouldOpenWindow] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const [airQualityData, setAirQualityData] = useState();
   const [postcode, setPostcode] = useState();
+
+  const [isInitialQuery, setIsInitialQuery] = useState(true);
 
   // TODO - customise this
   const options = {
@@ -121,6 +127,28 @@ export const Home = () => {
     }
   }, []);
 
+  const getPollenCount = useCallback(async (lat, lng) => {
+    const res = await fetch(
+      `https://api.ambeedata.com/latest/by-lat-lng?lat=${lat}&lng=${lng}`,
+      {
+        method: 'GET',
+        headers: {
+          'x-api-key':
+            '4f4249bcbb2f56cb1d360e237b69a88f9a9f4ed84d4bb5ec55f2966f4ef64777',
+          'Content-type': 'application/json',
+        },
+      }
+    );
+
+    if (res.ok === true) {
+      const { stations } = await res.json();
+
+      setAirQualityData(stations);
+
+      return stations;
+    }
+  }, []);
+
   const convertPostcodeToLatLon = useCallback(async postcode => {
     Geocode.setApiKey('AIzaSyD_G7EMILNLxo0NdXBtZnRS7QmIw0dZc4U');
     Geocode.setLanguage('en');
@@ -134,60 +162,112 @@ export const Home = () => {
   }, []);
 
   const handlePostcodeChange = useCallback(event => {
-    setShouldOpenWindow(undefined);
-
     setPostcode(event.target.value);
   }, []);
 
   const handlePostcodeSubmit = useCallback(async () => {
-    const res = await convertPostcodeToLatLon(postcode);
+    setIsInitialQuery(false);
+    setIsLoading(true);
+
+    const { lat, lng } = await convertPostcodeToLatLon(postcode);
 
     // TODO - put other fetches here
-    const data = await getAirQuality(res.lat, res.lng);
+    const airQualityData = await getAirQuality(lat, lng);
+    const pollenCount = await getPollenCount(lat, lng);
 
     // TODO - make a recommendation
-    setShouldOpenWindow(false);
+    setShouldOpenWindow(true);
+    setIsLoading(false);
   }, [convertPostcodeToLatLon, postcode, getAirQuality]);
 
   return (
-    <Box className="box" minH="100vh">
+    <Box className="box" minH="100vh" minW="800px">
       <Box textAlign="center" fontSize="xl">
-        <Box spacing={8}>
-          <h1 className="h1">Fresh Air Pro</h1>
-          <FormControl>
-            <FormLabel className="formLabel" htmlFor="postcodeInput">
-              Description...
-            </FormLabel>
-            <Input
-              id="postcodeInput"
-              value={postcode}
-              onChange={handlePostcodeChange}
-              placeholder="Enter a post code..."
-            />
-            <Button onClick={handlePostcodeSubmit}>Let's go</Button>
-          </FormControl>
+        { isInitialQuery ? (
+          <Box className="initialHomeContainer" spacing={8}>
+            <h1 className="h1">
+              breeze
+            </h1>
 
-          {shouldOpenWindow !== undefined && (
-            <>
-              {shouldOpenWindow ? (
-                <Text pt={10} bg="white">
-                  Open the window
-                </Text>
+            <Box className="pageContainer">
+              <Box className="descriptionContainer">
+                Help the planet (and your wallet) with breeze. <br/>
+                Find out when you can switch off the AC/fans and open the window.
+              </Box>
+
+              <Box className="postcodeFormContainer">
+                <FormControl>
+                  <div className="postcodeForm">
+                    <div className="postcodeInput">
+                      <FormLabel className="formLabel" htmlFor="postcodeInput">
+                        Enter your postcode
+                      </FormLabel>
+                      <Input
+                        id="postcodeInput"
+                        value={postcode}
+                        onChange={handlePostcodeChange}
+                        placeholder="E.g. SW1W 0NY"
+                      />
+                    </div>
+                    <Button onClick={handlePostcodeSubmit}>Let's go</Button>
+                  </div>
+                </FormControl>
+              </Box>
+            </Box>
+          </Box>
+        ) : (
+          <Box>
+            <Box className="topNav">
+              <Text className="navLogo">
+                breeze
+              </Text>
+
+              <Box>
+                <FormControl>
+                  <div className="navPostcodeForm">
+                    <div className="navPostcodeInput">
+                      <Input
+                        value={postcode}
+                        onChange={handlePostcodeChange}
+                        placeholder="E.g. SW1W 0NY"
+                      />
+                    </div>
+                    <Button onClick={handlePostcodeSubmit}>Let's go</Button>
+                  </div>
+                </FormControl>
+              </Box>
+            </Box>
+            { isLoading ? (
+                <Box className="loadingContainer">
+                  <BouncingDotsLoader/>
+                </Box>
               ) : (
-                <Text pt={10} bg="white">
-                  Don't open the window
-                </Text>
-              )}
+              <Box className="resultContainer">
+                {shouldOpenWindow ? (
+                  <Box className="openWindowResultContainer">
+                    <Text className="openWindowText">
+                      Open the window
+                    </Text>
+                    <Img className="openWindowImage" src={openWindowImage} />
+                  </Box>
+                ) : (
+                  <Text pt={10} bg="white">
+                    Don't open the window
+                  </Text>
+                )}
 
-              <Chart
-                options={options}
-                series={[airQualityData[0]['AQI']]}
-                type="radialBar"
-                width="500"
-              />
-            </>
-          )}
-        </Box>
+                <Box>
+                  <Chart
+                    options={options}
+                    series={[airQualityData[0]['AQI']]}
+                    type="radialBar"
+                    width="400"
+                  />
+                </Box>
+              </Box>
+            )}
+          </Box>
+        )}
       </Box>
     </Box>
   );
